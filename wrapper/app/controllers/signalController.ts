20b9050a8db45e3fa937ddfb8718b740b65885c3
Димитър Кацаров < >
2017-12-17 15:52:34 +0200

    wowo

--- wrapper/app/controllers/signalController.ts
+++ wrapper/app/controllers/signalController.ts
@@ -7,17 +7,15 @@ module MobileWrapper.Controllers {
         private scope: SignalScope;
         private state: any;
         private commonService: Services.CommonService;
-        private signal: any;
 
 
-        static $inject = ['$scope', 'commonService', '$state', 'signall'];
+        static $inject = ['$scope', 'commonService', '$state'];
 
-        constructor($scope: SignalScope, commonService: Services.CommonService, $state: any, signall: any) {
+        constructor($scope: SignalScope, commonService: Services.CommonService, $state: any) {
             this.scope = $scope;
             this.state = $state;
             this.commonService = commonService;
             this.commonService.SetMenuVisibility(true);
-            this.signal = signall;
             setInterval(function(){
               this.scope.timer++;
             }, 1000);
@@ -27,8 +25,8 @@ module MobileWrapper.Controllers {
           var signal = {
             Service: service,
             ThreatLevel: 2,
-            Location: "Sofia",
-            TimeSent: Date.now(),
+            Location: "Sofia, FMI / 42.674247, 23.330349",
+            TimeSent: new Date().toLocaleTimeString(),
             Victim: true,
             IMEI: "28bdd989ba275457"
           }
@@ -40,18 +38,32 @@ module MobileWrapper.Controllers {
         }
 
         public SetSignalLevel(threatLevel: any){
-          this.signal.ThreatLevel = threatLevel;
+          var signal = {
+            Service: "Police",
+            ThreatLevel: threatLevel,
+            Location: "Sofia, FMI / 42.674247, 23.330349",
+            TimeSent: new Date().toLocaleTimeString(),
+            Victim: true,
+            IMEI: "28bdd989ba275457"
+          }
 
-          $.ajax({type: "POST", data: this.signal, url: "http://192.168.43.151:3000/api/Signal/SendSignal?imei=28bdd989ba275457"})
+          $.ajax({type: "POST", data: signal, url: "http://192.168.43.151:3000/api/Signal/SendSignal?imei=28bdd989ba275457"})
           .then(() => {
             this.commonService.NavigateToPage('signal', this.state, this.scope);
           })
         }
 
         public CancelSignal(){
-          this.signal.ThreatLevel = 0;
+          var signal = {
+            Service: "Police",
+            ThreatLevel: 0,
+            Location: "Sofia, FMI / 42.674247, 23.330349",
+            TimeSent: new Date().toLocaleTimeString(),
+            Victim: true,
+            IMEI: "28bdd989ba275457"
+          }
 
-          $.ajax({type: "POST", data: this.signal, url: "http://192.168.43.151:3000/api/Signal/SendSignal?imei=28bdd989ba275457"})
+          $.ajax({type: "POST", data: signal, url: "http://192.168.43.151:3000/api/Signal/SendSignal?imei=28bdd989ba275457"})
           .then(() => {
             this.commonService.NavigateToPage('home', this.state, this.scope);
           })
