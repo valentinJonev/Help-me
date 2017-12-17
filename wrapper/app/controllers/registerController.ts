3e27d6cec1d5087aa09e56e7ea9113e1c1dcf431
Димитър Кацаров < >
2017-12-17 13:26:50 +0200

    register state

--- wrapper/app/controllers/registerController.ts
+++ wrapper/app/controllers/registerController.ts
@@ -1,54 +1,43 @@
 module MobileWrapper.Controllers {
     interface RegisterScope extends ng.IScope{
+      name: any,
+      mail: any,
+      idCard: any,
+      egn: any,
+      address: any,
+      drugs: any,
+      alergies: any,
+      height: any,
+      bloodType: any,
+      birthDate: any,
+      photo: any
     }
 
     export class RegisterController {
         private scope: RegisterScope;
         private commonService: Services.CommonService;
-        private picture: any;
+        private state: any;
 
-        static $inject = ['$scope', 'commonService'];
+        static $inject = ['$scope', '$state', 'commonService'];
 
-        constructor($scope: RegisterScope, commonService: Services.CommonService) {
+        constructor($scope: RegisterScope, $state: any, commonService: Services.CommonService) {
             this.scope = $scope;
+            this.state = $state;
             this.commonService = commonService;
             this.commonService.SetMenuVisibility(false);
-            this.setup();
         }
 
-        private setup(){
+        public RegisterUser(){
+            var userInfo = {Name: this.scope.name, Email: this.scope.mail,
+              BloodType: this.scope.bloodType, EGN: this.scope.egn, PhoneNumber: "+359000000000",
+              Alergies: (this.scope.alergies | "None"), DrugsUsed: (this.scope.drugs | "None"),
+               Sex: "Male", CurrentAddress: this.scope.address, IdCardNumber: this.scope.idCard,
+               Photo: this.scope.photo, Height: this.scope.height}
 
-          var fileInput  = document.getElementsByClassName( "input-file" )[0],
-              button     = document.getElementsByClassName( "input-file-trigger" )[0],
-              the_return = document.getElementsByClassName("file-return")[0],
-              preview    = document.getElementById('preview');
-
-          button.addEventListener( "keydown", function( event ) {
-              if ( event.keyCode == 13 || event.keyCode == 32 ) {
-                  fileInput.focus();
-              }
-          });
-          button.addEventListener( "click", function( event ) {
-             fileInput.focus();
-             return false;
-          });
-          fileInput.addEventListener( "change", function( event ) {
-              this.picture = this;
-              this.readURL(this);
-          });
-        }
-
-        private readURL(input) {
-
-          if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function(e) {
-              $('#preview').attr('src', e.target.result);
-            }
-
-            reader.readAsDataURL(input.files[0]);
-          }
+            $.ajax({type: "POST", data: userInfo, url: "192.168.43.151:3000/api/User/Register?imei=dszfgyt37rff87ew"})
+            .then(() => {
+              this.commonService.NavigateToPage('home', this.state, this.scope)
+            })
         }
     }
 }
